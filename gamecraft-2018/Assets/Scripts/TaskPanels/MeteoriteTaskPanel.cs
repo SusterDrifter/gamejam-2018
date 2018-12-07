5bb76bf875236d0cfb4fb5a9dfd8680e2244d856
zioul123 <zioul.rsmod123@gmail.com>
2018-12-07 22:51:22 +0800

    Add prefab and start the meteor task.

--- gamecraft-2018/Assets/Scripts/TaskPanels/MeteoriteTaskPanel.cs
+++ gamecraft-2018/Assets/Scripts/TaskPanels/MeteoriteTaskPanel.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeteoriteTaskPanel : TaskPanel {
+
+    public bool Solved = false;
+
+    //public Image TimerImage;
+    //public Button[] Buttons;
+    //public Text QuestionText;
+    //public Text AnswerText;
+
+    public CalculatorTask task;
+
+    public TaskSO t;
+
     public override void Display()
     {
     }
