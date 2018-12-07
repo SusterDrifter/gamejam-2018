7d16daa8988e5d7e6255b13e66b95c92b11adba2
zioul123 <zioul.rsmod123@gmail.com>
2018-12-08 03:30:57 +0800

    Add Signoff minigame.

--- gamecraft-2018/Assets/ScriptableObjects/Tasks/SignOffTaskSO.cs
+++ gamecraft-2018/Assets/ScriptableObjects/Tasks/SignOffTaskSO.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Task ScriptableObjects/Signoff")]
 public class SignOffTaskSO : TaskSO {
+    [SerializeField]
+    public bool needApproval;
+    [SerializeField]
+    public float timeToApproveMin = 3f;
+    [SerializeField]
+    public float timeToApproveMax = 7f;
 
     public override Task Create()
     {
