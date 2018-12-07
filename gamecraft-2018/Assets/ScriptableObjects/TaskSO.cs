002b6a98b2a52f684b9a1cbe845b54a09e8268ac
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-07 18:38:54 +0800

    Add min/max timeGiven properties to TaskSO

--- gamecraft-2018/Assets/ScriptableObjects/TaskSO.cs
+++ gamecraft-2018/Assets/ScriptableObjects/TaskSO.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public abstract class TaskSO : ScriptableObject {
 
+	[SerializeField]
+	private float maxTimeGiven = 10f;
+
+	[SerializeField]
+	private float minTimeGiven = 3f;
+
 	public abstract Task Create();
 
 }
