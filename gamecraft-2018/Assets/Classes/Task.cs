5c2f11badc3db426e4714b6f0f57269125e20cae
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-07 17:59:32 +0800

    Add properties to abstract class Task

--- gamecraft-2018/Assets/Classes/Task.cs
+++ gamecraft-2018/Assets/Classes/Task.cs
@@ -1,4 +1,24 @@
-﻿public class Task {
+﻿using UnityEngine;
 
+public abstract class Task {
 
+	[SerializeField]
+	private float timeRemaining;
+
+	[SerializeField]
+	private float timeGiven;
+
+	public abstract Task InitTaskWithTaskSO(TaskSO so);
+
+	public void Update() {
+		timeRemaining += Time.deltaTime;
+	}
+
+	private bool checkIfTaskExpire() {
+		if (timeRemaining <= 0) {
+			return false;
+		}
+
+		return true;
+	}
 }
