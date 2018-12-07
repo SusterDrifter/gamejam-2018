46a5c9e334bff900313b073dfc2718b6ba6af76f
zioul123 <zioul.rsmod123@gmail.com>
2018-12-08 05:46:31 +0800

    Add the temperature panel.

--- gamecraft-2018/Assets/Classes/TemperatureTask.cs
+++ gamecraft-2018/Assets/Classes/TemperatureTask.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TemperatureTask : Task {
-    
+
+    public bool Solved;
+
+    public float RateOfChange = 1f;
+    public float CurrentTemp = 32.5f;
+    public float TargetMin = 40f;
+    public float TargetMax = 45f;
+
 	public override void InitTaskWithTaskSO(TaskSO so)
     {
 		base.InitTaskWithTaskSO(so);
+        RateOfChange = ((TemperatureTaskSO)so).RateOfChange;
+        CurrentTemp = ((TemperatureTaskSO)so).StartingTemperature;
+        TargetMin = ((TemperatureTaskSO)so).TargetMin;
+        TargetMax = ((TemperatureTaskSO)so).TargetMax;
+        
     }
 
 	// Use this for initialization
@@ -15,7 +27,8 @@ public class TemperatureTask : Task {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public override void Update () {
+        CurrentTemp += RateOfChange * Time.deltaTime;
+        base.Update();
 	}
 }
--- gamecraft-2018/Assets/Scripts/TaskPanels/AirconTaskPanel.cs
+++ gamecraft-2018/Assets/Scripts/TaskPanels/AirconTaskPanel.cs
@@ -1,19 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AirconTaskPanel : TaskPanel {
+public class AirconTaskPanel : TaskPanel
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    public override void Display() {
+    public const float MIN_TEMP = 15f, MAX_TEMP = 50f;
 
+    public Image TimerImage;
+    public Button StopButton;
+    public Button LeftButton;
+    public Button RightButton;
+    public Image TempBar;
+    public Slider SliderMin;
+    public Slider SliderMax;
+
+    public TemperatureTask task;
+
+    public TaskSO t;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GameManager.instance.StartGame();
+        task = (TemperatureTask)TaskManager.instance.AddTaskWithSO(t);
+
+        TempBar.fillAmount = (task.CurrentTemp - MIN_TEMP) / 35;
+        SliderMin.value = task.TargetMin;
+        SliderMax.value = task.TargetMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!task.Solved)
+        {
+            TimerImage.fillAmount = task.TimeRemaining / task.TimeGiven;
+            TempBar.fillAmount = (task.CurrentTemp - MIN_TEMP) / 35;
+            if (task.TargetMin <= task.CurrentTemp && task.CurrentTemp <= task.TargetMax) {
+                TempBar.color = Color.green;
+            } else {
+                TempBar.color = Color.red;
+            }
+        }
+    }
+
+    public override void Display()
+    {
+
+    }
+
+    public void LeftButtonPressed()
+    {
+        if (task.RateOfChange > 0) {
+            task.RateOfChange *= -1;
+        }
+    }
+    public void RightButtonPressed()
+    {
+        if (task.RateOfChange < 0) {
+            task.RateOfChange *= -1;
+        }
+    }
+    public void StopButtonPressed()
+    {
+        if (task.TargetMin <= task.CurrentTemp && task.CurrentTemp <= task.TargetMax) {
+            task.Solved = true;
+        }
     }
 }
