e89239966ec2ffaa80e8a0f8ceefea014dd8ed2f
zioul123 <zioul.rsmod123@gmail.com>
2018-12-07 20:00:43 +0800

    Calculator game: Able to click all buttons.

--- gamecraft-2018/Assets/Scripts/TaskPanels/CalculatorTaskPanel.cs
+++ gamecraft-2018/Assets/Scripts/TaskPanels/CalculatorTaskPanel.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CalculatorTaskPanel : TaskPanel {
+    public const int ERASE = 10;
+    public const int MAX_LENGTH = 4;
 
+
+    public Image TimerImage;
     public Button[] Buttons;
     public Text QuestionText;
     public Text AnswerText;
 
+    public CalculatorTask task;
+
 	// Use this for initialization
 	void Start () {
-		
+        for (int i = 0; i < Buttons.Length; i++) {
+            int j = i;
+            Button b = Buttons[i];
+            b.onClick.AddListener(() => this.PressButton(j));
+        }
 	}
 	
 	// Update is called once per frame
@@ -20,5 +30,17 @@ public class CalculatorTaskPanel : TaskPanel {
 	}
     public override void Display() {
 
+    }
+
+    public void PressButton(int i) {
+        if (i == ERASE) {
+            if (AnswerText.text.Length == 0) return;
+            AnswerText.text = AnswerText.text.Substring(0, AnswerText.text.Length - 1);
+            return;
+        }
+
+        if (AnswerText.text.Length == MAX_LENGTH) return;
+
+        AnswerText.text += i;
     }
 }
