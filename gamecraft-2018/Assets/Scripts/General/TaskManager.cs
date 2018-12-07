4ef450c918f8cc5dab3f756a116b6429a75a98e4
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-07 18:03:21 +0800

    Make TaskManager a singleton wit basic Task management

--- gamecraft-2018/Assets/Scripts/General/TaskManager.cs
+++ gamecraft-2018/Assets/Scripts/General/TaskManager.cs
@@ -1,16 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class TaskManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	static object _lock = new object();
+
+    private static TaskManager _taskManager;
+
+    public static TaskManager instance
+    {
+        get
+        {
+            if (_taskManager == null)
+            {
+                lock (_lock)
+                    if (_taskManager == null)
+                        _taskManager = new TaskManager();
+            }
+            return _taskManager;
+        }
+    }
+
+
+    private void Awake()
+    {
+        if (!_taskManager)
+        {
+            _taskManager = FindObjectOfType<TaskManager>();
+            if (_taskManager)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        if (instance == null)
+        {
+            _taskManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+                Destroy(gameObject);
+        }
+    }
+
+	[SerializeField]
+	private List<Task> ongoingTasks;
+
+	[SerializeField]
+	private int maxTasks;
+
+	public void AddTaskWithSO(TaskSO so) {
+		Task task = so.Create();
+
+		ongoingTasks.Add(task);
+	}
+
+	public void CompleteTask(Task task) {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update()
+	{
+		ongoingTasks.ForEach((Task obj) => obj.Update());
 	}
 }
