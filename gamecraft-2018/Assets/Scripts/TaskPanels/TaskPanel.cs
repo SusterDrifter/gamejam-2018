4508901331eb250bfa8c2e37dffa74c09052f4ff
zioul123 <zioul.rsmod123@gmail.com>
2018-12-07 18:45:13 +0800

    Modify PanelController.

--- gamecraft-2018/Assets/Scripts/TaskPanels/TaskPanel.cs
+++ gamecraft-2018/Assets/Scripts/TaskPanels/TaskPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class TaskPanel : MonoBehaviour {
-
+    [SerializeField]
     public TaskStub task;
 
     public abstract void Display();
