1c04dd77082a1c43e8bffb74fadf0cb0db849c0b
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-07 21:09:02 +0800

    Add TaskCellController class

---
+++ gamecraft-2018/Assets/TaskCellController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TaskCellController : MonoBehaviour {
+
+	[SerializeField]
+	public Image backgroundImage;
+
+	[SerializeField]
+	public Slider timerSlider;
+
+	[SerializeField]
+	public Text taskNameText;
+
+	[SerializeField]
+	public Text hotkeyText;
+
+	public void SetupWithTask(Task task) {
+		timerSlider.maxValue = task.TimeGiven;
+		//backgroundImage.sprite = ;
+		taskNameText.text = task.TaskSO.taskName;
+	}
+
+}
