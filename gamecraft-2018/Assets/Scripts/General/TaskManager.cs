4278bdc2d30c2c9e0ee9b630a084e3695036792b
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-07 20:04:40 +0800

    Add TaskManager lifecycle methods

--- gamecraft-2018/Assets/Scripts/General/TaskManager.cs
+++ gamecraft-2018/Assets/Scripts/General/TaskManager.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TaskManager : MonoBehaviour {
-
+	
+#region Singleton
 	static object _lock = new object();
 
     private static TaskManager _taskManager;
@@ -20,9 +22,13 @@ public class TaskManager : MonoBehaviour {
             return _taskManager;
         }
     }
+	#endregion
+   
+	Coroutine taskAddCoroutine;
 
+	StageSO currentStage;
 
-    private void Awake()
+	private void Awake()
     {
         if (!_taskManager)
         {
@@ -57,11 +63,55 @@ public class TaskManager : MonoBehaviour {
 	}
 
 	public void CompleteTask(Task task) {
-		
+		ongoingTasks.Remove(task);
+	}
+    
+	IEnumerator _TaskAddCoroutine(StageSO stage)
+    {
+		float runningTime = 0;
+
+		while (true) {
+			runningTime += Time.deltaTime;
+
+			if (runningTime >= 1/stage.timeBetweenTaskAdd) {
+				AddTaskWithSO(getRandomTaskSO());
+				runningTime = 0;
+			}
+
+			yield return null;
+		}
+
+        yield return null;
+    }
+
+	public void StartStage(StageSO stage) {
+		StartTaskAddCoroutine(stage);
+	}
+
+	private void StartTaskAddCoroutine(StageSO stage) {
+		taskAddCoroutine = StartCoroutine(_TaskAddCoroutine(stage));
+	}
+
+	public void FlushTaskManager() {
+		StopCoroutine(taskAddCoroutine);
+		taskAddCoroutine = null;
+        // maybe iterate through ongoingTasks to clear them up properly.
+		ongoingTasks.Clear();
+	}
+
+	private TaskSO getRandomTaskSO() {
+		List<TaskSO> tasks = currentStage.tasks;
+
+		if (tasks.Count <= 0) {
+			return null;
+		}
+
+		return tasks[Random.Range(0, tasks.Count)];
 	}
 
 	private void Update()
 	{
 		ongoingTasks.ForEach((Task obj) => obj.Update());
 	}
+
 }
