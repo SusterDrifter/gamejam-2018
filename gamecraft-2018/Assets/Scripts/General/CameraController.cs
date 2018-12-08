64fd570fa9987a399cea36a9c6215bef06a6e9a0
zioul123 <zioul.rsmod123@gmail.com>
2018-12-08 11:58:06 +0800

    Bug fix.

--- gamecraft-2018/Assets/Scripts/General/CameraController.cs
+++ gamecraft-2018/Assets/Scripts/General/CameraController.cs
@@ -28,7 +28,7 @@ public class CameraController : MonoBehaviour {
 			case ShipPanel.Default:
 				MoveToPosition(new Vector3(0, 0, -10));
 				TweenToCameraSize(6.78f);
-                panelController.SwitchTask(-1, null);
+                //panelController.SwitchTask(-1, null);
 				break;
 			case ShipPanel.Calculator:
 				MoveToPosition(new Vector3(-6.52f, -3.68f, -10));
