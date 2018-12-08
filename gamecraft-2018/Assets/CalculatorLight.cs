d5695967b1ab693ae2b7a8b588b5e64ecb5df2ee
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-12-08 08:27:09 +0800

    Add env effect

---
+++ gamecraft-2018/Assets/CalculatorLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CalculatorLight : MonoBehaviour {
+
+	[SerializeField]
+	SpriteRenderer renderer;
+    
+	private void Awake()
+	{
+		renderer = GetComponent<SpriteRenderer>();
+	}
+    
+	private void Start()
+	{
+		fadeOut();
+	}
+
+	void fadeIn() {
+		renderer.DOFade(0.2f, 1.0f).OnComplete(() => fadeOut());
+	}
+
+	void fadeOut() {
+		renderer.DOFade(0.0f, 1.0f).OnComplete(() => fadeIn());
+	}
+
+}
